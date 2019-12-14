883fb980796ef1456dfd4fedb26ac494ec4fbe94
f-person <agakaryan.arshak@gmail.com>
2019-12-14 22:54:24 +0400

    Also add omitempty tag

--- bfj.py
+++ bfj.py
@@ -1,6 +1,17 @@
 import re
 import sys
 
+omitempty_in_json = True
+
+def omitempty_tag(jsonMode=False):
+    if jsonMode:
+        if omitempty_in_json:
+            return ",omitempty"
+        else:
+            return ""
+    
+    return ",omitempty"
+
 if len(sys.argv) == 1:
     go_struct = sys.stdin.read()
 else:
@@ -9,6 +20,7 @@ else:
 json_tags = re.findall('json:"([^"]*)"', go_struct)
 
 for m in json_tags:
-    go_struct = go_struct.replace('json:\"%s\"' % m, 'json:\"%s\" bson:\"%s\"' % (m, m))
+    go_struct = go_struct.replace('json:\"%s\"' % m, 'json:\"%s%s\" bson:\"%s%s\"' % (m, omitempty_tag(True), m, omitempty_tag()))
+
 
 print(go_struct)
